06840ce6ff1830db6cf9d104ccf7de961b060587
sonali dimble <sonali.dimble@techdenovo.com>
2021-07-14 11:49:59 -0700

    variable added...

--- Demo.java
+++ Demo.java
@@ -1,5 +1,6 @@
 class Demo{
    int a;
+   int b;
    public static void main(String[] args) {
       System.out.println("new line is added..."); 
       System.out.println("Demo class created....");
