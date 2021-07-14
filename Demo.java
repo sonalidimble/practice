b1b484f13ad3e6bb0ba680e8ee0175812faa4d41
sonali dimble <sonali.dimble@techdenovo.com>
2021-07-14 11:28:33 -0700

    demo file is created...

---
+++ Demo.java
@@ -0,0 +1,8 @@
+class Demo{
+   int a;
+   public static void main(String[] args) {
+      System.out.println("new line is added..."); 
+      System.out.println("Demo class created....");
+   }
+   
+}
